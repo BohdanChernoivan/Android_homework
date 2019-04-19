fed5a18f0e3bd11d44cad47d1b27816323ccd4ab
GodDAN <scoliztur@gmail.com>
2019-04-19 15:10:50 +0300

    nw

--- Dz_24/app/src/main/java/com/example/dz_24/MainActivity.java
+++ Dz_24/app/src/main/java/com/example/dz_24/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.dz_24;
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ValueAnimator valueAnimator;
-    ObjectAnimator objectAnimator;
+
+    Animation animation;
     ImageButton imgBtmHome;
     ImageButton imgBtmFavorites;
     ImageButton imgBtmSetting;
@@ -33,41 +34,22 @@ public class MainActivity extends AppCompatActivity {
         imgBtmHome = findViewById(R.id.one_btm);
         imgBtmFavorites = findViewById(R.id.two_btm);
         imgBtmSetting = findViewById(R.id.three_btm);
+        animation = AnimationUtils.loadAnimation(this, R.anim.decrease);
     }
 
     private void initAnimHome(View view) {
         Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
-        objectAnimator = ObjectAnimator.ofFloat(imgBtmHome, view.getResources().getResourceName(R.anim.decrease), 100f);
-        objectAnimator.setDuration(200);
-        objectAnimator.start();
+        imgBtmHome.startAnimation(animation);
     }
 
     private void initAnimFavorites(View view) {
         Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show();
-        valueAnimator = ValueAnimator.ofFloat(0f, 100f);
-        valueAnimator.setDuration(200);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float anim = (float) animation.getAnimatedValue();
-                imgBtmFavorites.setTranslationX(anim);
-            }
-        });
-        valueAnimator.start();
+        imgBtmFavorites.startAnimation(animation);
     }
 
     private void initAnimSetting(View view) {
         Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
-        valueAnimator = ValueAnimator.ofFloat(0f, 100f);
-        valueAnimator.setDuration(200);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float anim = (float) animation.getAnimatedValue();
-                imgBtmSetting.setScaleY(anim);
-            }
-        });
-        valueAnimator.start();
+        imgBtmSetting.startAnimation(animation);
     }
 
 }
